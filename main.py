0e7d2a2febc8ed5e72e900cfe4b30433c06e82dd
idel <little1student@gmail.com>
2025-09-11 12:47:09 +0300

    fix: match to example

--- main.py
+++ main.py
@@ -1,36 +1,28 @@
 import streamlit as st
 from transformers import pipeline
 from PIL import Image
-import io
-
-# Инициализация модели для классификации изображений (вынесена из условия для оптимизации)
-@st.cache_resource
-def load_classifier():
-    return pipeline("image-classification", model="google/vit-base-patch16-224")
-
-# Заголовок приложения
+#Заголовок приложения
 st.title("Распознавание изображений с помощью Hugging Face")
 
-# Добавляем блок с загрузкой изображения от пользователя
-uploaded_file = st.file_uploader("Загрузите изображение", type=["jpg", "jpeg", "png"])
+#Добавим ссылку на чат в телеграм
+url = "https://t.me/+qHHDOyGAj9llM2Qy"
+st.write("Ссылка для перехода в чат с материалами [тык](%s)" % url)
 
-# Проверка, загружено ли изображение
+#Добавляем блок с загрузкой изображения от пользователя
+uploaded_file = st.file_uploader("Загрузите изображение", type = ["jpg", "jpeg", "png"])
+
+#Проверка, загружено ли изображение
 if uploaded_file is not None:
-    try:
-        # Открываем изображение с помощью PIL и выводим на экран
-        image = Image.open(uploaded_file)
-        st.image(image, caption="Загруженное изображение", use_container_width=True)
-        
-        # Загружаем модель
-        classifier = load_classifier()
-        
-        # Распознавание изображения и запись результатов
-        with st.spinner("Распознаю изображение..."):
-            results = classifier(image)
-        
-        st.write("**Результаты распознавания:**")
-        for result in results:
-            st.write(f"{result['label']}: {result['score']:.2f}")
-            
-    except Exception as e:
-        st.error(f"Ошибка при обработке изображения: {str(e)}")
+    # Открываем изображение с помощью PIL и выводим на экран
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Загруженное изображение", use_container_width = True)
+
+    # Инициализация модели для классификации изображений
+    classifier = pipeline("image-classification", model="google/vit-base-patch16-224")
+
+    # Распознавание изображения и запись результатов
+    results  = classifier(image)
+
+    st.write("**Результаты распознавания**")
+    for result in results:
+        st.write(f"{result['label']}: {result['score']:.2f}")
